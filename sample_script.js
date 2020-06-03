e61d6ad263b3d93732c78c68fd3eb12d0bdd2910
krishnavei <krishnaveni.b@cawstudios.com>
2020-06-04 01:29:30 +0530

    listening to DOMSubtreeModified

--- sample_script.js
+++ sample_script.js
@@ -1,3 +1,56 @@
+//chrome.runtime.sendMessage({ todo: "showfb" });
+
+// var selector = document.querySelector(
+//   "#groupsUnifiedQueueLHCTabs > div > ul > li:nth-child(4) > div > a"
+// );
+
+// $(document).ready(function () {
+//   $("#groupsUnifiedQueueLHCTabs div ul li:nth-child(4) div a").click(
+//     function () {
+//       checkPageLoad();
+//     }
+//   );
+// });
+
+// $(document).ready(function () {
+//   $(document).on(
+//     "click",
+//     "#groupsUnifiedQueueLHCTabs div ul li:nth-child(4) div a",
+//     function () {
+//       checkPageLoad();
+//     }
+//   );
+// });
+
+// var observer = new MutationObserver(function (mutations, observer) {
+//   console.log(mutations, observer);
+//   console.log("Hi");
+// });
+// observer.observe(document, {
+//   subtree: true,
+//   characterData: true,
+// });
+
+// document.querySelector("#mainContainer").addEventListener("load", function () {
+//   console.log("Page is loaded");
+// });
+// $("#mainContainer").on("DOMSubtreeModified", function () {
+//   if (document.getElementById("member_requests_pagelet")) {
+//     if (
+//       document
+//         .querySelectorAll("#member_requests_pagelet ._3k4n._4-u3")[2]
+//         .querySelectorAll(".clearfix").length > 0
+//     ) {
+//       console.log("matched");
+//     }
+//   }
+// });
+// document.body.addEventListener("DOMSubtreeModified", function () {
+//   if (document.getElementById("member_requests_pagelet")) {
+//     console.log("Hi");
+//   }
+// });
+// selector.addEventListener("click", checkPageLoad());
 function checkPageLoad() {
   console.log("I'm here");
   if (document.getElementById("member_requests_pagelet")) {
@@ -12,30 +65,35 @@ function checkPageLoad() {
   }
 }
 
-$(document).on("DOMSubtreeModified", function () {
+$("#mainContainer").on("DOMSubtreeModified", function () {
   if (document.getElementById("member_requests_pagelet")) {
-    try {
+    if (
+      document
+        .querySelectorAll("#member_requests_pagelet ._3k4n._4-u3")[2]
+        .querySelectorAll(".clearfix").length > 0
+    ) {
       if (
         document
           .querySelectorAll("#member_requests_pagelet ._3k4n._4-u3")[2]
-          .querySelectorAll(".clearfix").length > 0
+          .querySelectorAll(".clearfix")[0]
+          .querySelectorAll("._4wsp._51xa")[0]["firstElementChild"].innerHTML ==
+        "Approve"
       ) {
-        if (
-          document
-            .querySelectorAll("#member_requests_pagelet ._3k4n._4-u3")[2]
-            .querySelectorAll(".clearfix")[0]
-            .querySelectorAll("._4wsp._51xa")[0]["firstElementChild"]
-            .innerHTML == "Approve"
-        ) {
-          console.log("matched");
-          checkPageLoad();
-        }
+        console.log("matched");
+        checkPageLoad();
       }
-    } catch (error) {
-      console.log("Not Loaded");
     }
   }
 });
+
+// function checkPageLoad() {
+//   console.log("I'm here");
+//   if (document.getElementById("#member_requests_pagelet")) {
+//     console.log("Hi");
+//     change_buttons();
+//     scrap_all_mem_details();
+//   }
+// }
 
 function change_buttons() {
   // change Approve all button
@@ -64,6 +122,15 @@ function change_buttons() {
         "#member_requests_pagelet ._3k4n._4-u3:nth-child(1) .clearfix ._51xa button:nth-child(2)"
       ).click();
     });
+    // change all Approve buttons
+    for (i = 0; i < count; i++) {
+      document
+        .querySelectorAll("#member_requests_pagelet ._3k4n._4-u3")[2]
+        .querySelectorAll(".clearfix")
+        [i].querySelectorAll("._4wsp._51xa")[0]["firstElementChild"].innerHTML =
+        "Approve by grepInfo";
+    }
+  } else if (count == 1) {
     // change all Approve buttons
     for (i = 0; i < count; i++) {
       document
